e6f99655e28df82aab4b6386a0ce0b87c19d051b
diyakallarackal <diyakallarackal@gmail.com>
2024-10-19 15:53:18 +0530

    Add files via upload

---
+++ basiciprogram6.py
@@ -0,0 +1,13 @@
+""""
+Author:Diya Pradeep
+Date:19-10-2024
+Description:Simple desktop calculator using Python. Only the five basic arithmetic operators.
+"""
+firstname=str(input("Enter first name:"))
+secondname=str(input("Enter second name:"))
+str_name=firstname+" "+secondname
+print(str_name)
+len_firstname=len(firstname)
+print(str_name[:len_firstname])
+
+
