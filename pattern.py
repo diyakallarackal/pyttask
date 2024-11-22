2376ab3c63039a7b3fd95811de458d978f76c7d3
diyakallarackal <diyakallarackal@gmail.com>
2024-11-22 16:00:04 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,43 @@
+'''
+Name:Diya Pradeep
+Date:22-11-2024
+Description:Program to construct patterns of stars (*), using a nested for loop.
+'''
+row=int(input("Enter number of rows:"))
+print("Increasing Triangle")
+for i in range(0,row):
+    for j in range(0, i + 1):
+        print("*", end=" ")
+    print(' ')
+
+
+row=int(input("Enter number of rows:"))
+print("Decreasing Triangle")
+for i in range(row,0,-1):
+    for j in range(0,i):
+        print("*", end=" ")
+    print('  ')
+
+
+row=int(input("Enter number of rows:"))
+print("Hill Pattern")
+for i in range(1,row):
+    for j in range(row-i):
+        print(" ", end=" ")
+    for k in range(2*i-1):
+        print("*", end=" ")
+    print('  ')
+
+
+row=int(input("Enter number of rows:"))
+print("Decreasing Hill Pattern")
+for i in range(row,0,-1):
+    for j in range(row-i,0,-1):
+        print(" ", end=" ")
+    for k in range(2*i-1):
+        print("*", end=" ")
+    print('  ')
+
+
+
+
