8128c49f03c0c544c06460bac6ef20e720aac8fc
diyakallarackal <diyakallarackal@gmail.com>
2024-12-06 15:21:23 +0530

    Create fibonaccinumbers2.py

---
+++ fibonaccinumbers2.py
@@ -0,0 +1,4 @@
+import fibonaccinumbers
+n=int(input("Enter the no of fibonacci numbers to be generate:"))
+result=fibonaccinumbers.fibonacci_num(n)
+print("The first ",n," fibonacci numbers are :",result)
